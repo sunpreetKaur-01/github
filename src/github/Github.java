27ee6e5942f9fa792a1c11efa2e94923936a8151
sunpreetkaur-01 <kaur7059@sheridancollege.ca>
2020-06-07 02:38:03 -0400

    message 2

--- src/github/Github.java
+++ src/github/Github.java
@@ -17,6 +17,7 @@ public class Github {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("github");
+        System.out.println("changes");
     }
     
 }
